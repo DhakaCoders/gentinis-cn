9a6973f96ee61686319c5b1c38a88359206dd066
Rannojit <cserannojitsarkar22@gmail.com>
2020-05-16 17:25:53 +0600

    inner page 90% desktop view done

--- themes/gentinis/assets/js/main.js
+++ themes/gentinis/assets/js/main.js
@@ -212,4 +212,46 @@ var containerExtraWidthCalMinusPx = containerExtraWidthCal - 20;
 
 $('.page-banner-img-cntlr').css("right", containerExtraWidthCalMinusPx);
 
+
+
+if( $('.dftResponsibilityGrdsSlider').length ){
+    $('.dftResponsibilityGrdsSlider').slick({
+      dots: false,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+}
+
+
+
 })(jQuery);
